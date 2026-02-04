d68373e723b24fab5057263a4732ded2e8e618e9
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-05 00:47:05 +0530

    first commit

---
+++ solidRectangle.java
@@ -0,0 +1,20 @@
+class solidRectangle {
+
+    public static void main(String[] args) {
+
+        int rows = 4;
+        int columns = 5;
+
+        // * * * * * * *
+        // * * * * * * *
+        // * * * * * * *
+        // * * * * * * *
+
+        for(int i = 1; i<= rows; i++){
+            for(int j = 1; j<= columns; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+} 
