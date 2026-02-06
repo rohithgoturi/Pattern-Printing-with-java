529fd98a3a9ceec3c21337b75b78f49cf7dc09c5
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-06 18:31:13 +0530

    pascals triangle using loops

---
+++ pascalsTriangle.java
@@ -0,0 +1,22 @@
+public class pascalsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+
+            // spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // numbers
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+    }
+}
